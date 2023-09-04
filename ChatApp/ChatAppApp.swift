927880e2e91c4467bee1793677ac0154bdab6153
Kevin Heryanto <heryantokevin@gmail.com>
2023-09-04 23:30:41 +0700

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Kevin Heryanto on 04/09/2566 BE.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
